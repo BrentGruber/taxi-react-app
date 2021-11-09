14449c4ee28033ce6f7eb4eae7ba0bfb6a0cd7d7
BrentGruber <brent.gruber77@gmail.com>
2021-11-08 23:40:17 -0500

    users and trips working http endpoints

--- server/trips/views.py
+++ server/trips/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions, viewsets
+from rest_framework_simplejwt.views import TokenObtainPairView
 
-# Create your views here.
+from .models import Trip
+from .serializers import LogInSerializer, UserSerializer, TripSerializer
+
+
+
+class SignUpView(generics.CreateAPIView):
+    queryset = get_user_model().objects.all()
+    serializer_class = UserSerializer
+
+
+class LogInView(TokenObtainPairView):
+    serializer_class = LogInSerializer
+
+
+class TripView(viewsets.ReadOnlyModelViewSet):
+    lookup_field = 'id'
+    lookup_url_kwarg = 'trip_id'
+    permission_classes = (permissions.IsAuthenticated,)
+    queryset = Trip.objects.all()
+    serializer_class = TripSerializer
