913ca941c17db8c4540ba7f4300e16a6912fcda5
BrentGruber <brent.gruber77@gmail.com>
2021-11-23 23:51:47 -0500

    adding docker-compose

--- client/cypress/integration/authentication.spec.js
+++ client/cypress/integration/authentication.spec.js
@@ -30,6 +30,18 @@ describe('Authentication', function () {
     });
 
     it('Can sign up.', function() {
+        cy.intercept('POST', 'sign_up', {
+            statusCode: 201,
+            body: {
+                'id': 1,
+                'username': 'gary.cole@example.com',
+                'first_name': 'Gary',
+                'last_name': 'Cole',
+                'group': 'driver',
+                'photo': '/media/images/photo.jpg'
+            }
+        }).as('signUp');
+
         cy.visit('/#/sign-up');
         cy.get('input#username').type('gary.cole@example.com');
         cy.get('input#firstName').type('Gary');
@@ -38,6 +50,7 @@ describe('Authentication', function () {
         cy.get('select#group').select('driver');
         cy.get('input#photo').attachFile('images/photo.jpg');
         cy.get('button').contains('Sign Up').click();
+        cy.wait('@signUp');
         cy.hash().should('eq', '#/log-in');
     });
 
@@ -75,6 +88,31 @@ describe('Authentication', function () {
         );
         cy.hash().should('eq', '#/log-in');
     });
+
+    it('Show invalid fields on sign up error.', function () {
+        cy.intercept('POST', 'sign_up', {
+            statusCode: 400,
+            body: {
+                'username': [
+                    'A user with that username already exists.'
+                ]
+            }
+        }).as('signUp');
+        cy.visit('/#/sign-up');
+        cy.get('input#username').type('gary.cole@example.com');
+        cy.get('input#firstName').type('Gary');
+        cy.get('input#lastName').type('Cole');
+        cy.get('input#password').type('pAssw0rd', {log:false});
+        cy.get('select#group').select('driver');
+
+        cy.get('input#photo').attachFile('images/photo.jpg');
+        cy.get('button').contains('Sign Up').click();
+        cy.wait('@signUp');
+        cy.get('div.invalid-feedback').contains(
+            'A user with that username already exists'
+        );
+        cy.hash().should('eq', '#/sign-up');
+    })
 
     it('Can log out.', function () {
         logIn();
